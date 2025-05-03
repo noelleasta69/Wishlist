94e0e33ed7d7610ceab0061a8ee573da91d34154
noelleasta69 <obitouchihathesageofsixpaths@gmail.com>
2025-05-03 18:42:50 +0530

    added route to invite friends

--- wishlist-backend/src/routes/wishlist.routes.ts
+++ wishlist-backend/src/routes/wishlist.routes.ts
@@ -1,14 +1,23 @@
 import { Router } from 'express';
 import { authenticate } from '../middlewares/auth';
-import { createWishlist, getAllWishlists } from '../controllers/wishlist.controller';
+import { createWishlist, getAllWishlists, inviteUserToWishlist } from '../controllers/wishlist.controller';
+import { validate } from '../middlewares/validate';
+import { inviteUserSchema } from '../validators/invite.schema';
 
 const router = Router();
 
+//@ts-ignore
+router.use(authenticate)
+
 // POST /api/wishlists -> Create a new wishlist
 //@ts-ignore == > fix these issues later;
-router.post('/', authenticate, createWishlist);
+router.post('/', createWishlist);
 //@ts-ignore == > fix these issues later
 // GET /api/wishlists -> Get all wishlists of the logged-in user
-router.get('/', authenticate, getAllWishlists);
+router.get('/', getAllWishlists);
+
+console.log("hi"); 
+//@ts-ignore == > fix these issues later
+router.post('/:wishlistId/invite',validate(inviteUserSchema.body),inviteUserToWishlist);
 
 export default router;
